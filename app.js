4a5adf3b45a2577ae3459c9127517c21c6b1467c
Dave Townsend <townsend@patriot.net>
2016-06-26 19:14:29 -0400

    Hardwired scenario list screen

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var mainMenu = {
+var mainScreen = {
     view: function() {
         return m("ul", [
-                   m("li", m("a[href='#1']", {config: m.route}, "Figures")),
-                   m("li", m("a[href='#2']", {config: m.route}, "Scenarios")),
+                   m("li", m("a[href='#']", {config: m.route}, "Figures")),
+                   m("li", m("a[href='/scenarios']", {config: m.route}, "Scenarios")),
                  ]);
+    }
+};
+
+var scenarioListScreen = {
+    view: function() {
+        return m("table", [
+            m("tr", [m("td", "Scenario 1")]),
+            m("tr", [m("td", "Scenario 2")]),
+                  ]);
     }
 };
 
 m.route.mode = "hash";
 m.route(document.getElementById("mainDiv"), "/", {
-    "/": mainMenu
+    "/": mainScreen,
+    "/scenarios": scenarioListScreen
 });
