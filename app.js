1c696c4eae0cbfd37e1ac0edded7d36218bccc65
Dave Townsend <townsend@patriot.net>
2016-08-08 19:26:06 -0400

    Scenario Details has completion pie charts

--- app.js
+++ app.js
@@ -80,6 +80,51 @@ var requestWrapper = function(opts) {
     };
 };
 
+var Pie = {
+    view: function(ctrl, size, n, nPainted, nOwned) {
+        var circleAttrs = { cx: size/2, cy: size/2, r: size/2-2, fill: '#ccc' };
+        var pctPainted  = Math.min(n > 0 ? nPainted / n : 0, 1.0);
+        var pctOwned    = Math.min(n > 0 ? nOwned   / n : 0, 1.0);
+
+        // Use the most appropriate base cirle color
+        if (pctPainted == 1.0) {
+            circleAttrs.fill = '#0c0';
+        } else if (pctOwned == 1.0) {
+            circleAttrs.fill = '#cc0';
+        }
+
+        return m("svg", { width: size, height: size}, [
+            m("circle", circleAttrs),
+            Pie.slice(circleAttrs, 0, pctPainted, "#0c0"),
+            Pie.slice(circleAttrs, pctPainted, pctOwned, "#dd0")
+        ]);
+    },
+
+    slice: function slice(circleAttrs, pctStart, pctEnd, fill) {
+        // No slices at 0% or 100%
+        if (pctStart >= pctEnd || (pctStart == 0.0 && pctEnd == 1.0)) {
+            return null;
+        }
+
+        var pathParts = [];
+        pathParts.push("M" + circleAttrs.cx + "," + circleAttrs.cy);
+        pathParts.push("L" +
+                       (circleAttrs.cx + (Math.sin(pctStart * 2 * Math.PI) * circleAttrs.r)) + "," +
+                       (circleAttrs.cy - (Math.cos(pctStart * 2 * Math.PI) * circleAttrs.r)));
+        pathParts.push("A" + circleAttrs.r + "," + circleAttrs.r);
+        pathParts.push("0");  // x-axis rotate
+        pathParts.push((pctEnd - pctStart >= .5 ? "1" : "0") + ",1"); // long-arc, clockwise
+        pathParts.push((circleAttrs.cx + (Math.sin(pctEnd * 2 * Math.PI) * circleAttrs.r)) + "," +
+                       (circleAttrs.cy - (Math.cos(pctEnd * 2 * Math.PI) * circleAttrs.r)));
+        pathParts.push("z");
+
+        return m("path", {
+            d: pathParts.join(" "),
+            style: "fill: " + fill + "; fill-opacity: 1; stroke: black; stroke-width: 1"
+        });
+    }
+};
+
 //==================================================================================================================================
 var MainScreen = {
     view: function() {
@@ -250,6 +295,7 @@ var ScenarioDetailScreen = {
         if (figuresList != null) {
             figuresList.forEach(function(f) {
                 figs.push(m("div.figure-line", [
+                    m(Pie, 24, f.amount, f.num_painted, f.num_owned),
                     f.amount > 1 ? m("div.figure-line-amount", f.amount) : null,
                     m("div.figure-line-name", f.name)
                 ]));
