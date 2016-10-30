3424243fc485af7feaad4bb84c6cf05764ba4a4a
Dave Townsend <townsend@patriot.net>
2016-10-30 09:53:53 -0400

    Request modularized

---
+++ app/request.js
@@ -0,0 +1,82 @@
+/* global require module */
+
+var m           = require("mithril");
+var Credentials = require("credentials");
+var LoginScreen = require("sbg").LoginScreen;
+
+var Request = (function() {
+    var failFn = (resp, errorComponent) => {
+        var resolvedComponent = errorComponent || LoginScreen;
+        if (resp === null) {
+            resolvedComponent.setError("The server appears to be down. Please try again later.");
+        } else {
+            resolvedComponent.setError(resp.errors);
+        }
+        if (errorComponent) {
+            m.route();
+        } else {
+            m.route("/login");
+        }
+    };
+
+    return {
+        get: function(url, successFn, errorComponent) {
+            var opts = { method: "GET", url: url };
+            if (Credentials.token()) {
+                opts.config = function(xhr) { xhr.setRequestHeader("authorization", "Token token=" + Credentials.token()); };
+            }
+            return m.request(opts).then(
+                resp => successFn(resp),
+                resp => failFn(resp, errorComponent)
+            );
+        },
+
+        post: function(url, data, successFn, errorComponent) {
+            var opts = { method: "POST", url: url, data: data };
+            if (Credentials.token()) {
+                opts.config = function(xhr) { xhr.setRequestHeader("authorization", "Token token=" + Credentials.token()); };
+            }
+            return m.request(opts).then(
+                resp => successFn(resp),
+                resp => failFn(resp, errorComponent)
+            );
+        }
+    };
+}());
+
+module.exports = Request;
+
+/*******
+// courtesy http://ratfactor.com/daves-guide-to-mithril-js
+var requestWrapper = function(opts) {
+    return new function() {
+        var me = this;
+        me.opts = opts;
+        me.success = me.loading = me.failed = false;
+        me.errorStatus = me.errorBody = "";
+        me.data = null;
+        me.opts.background = true;
+        me.opts.extract = function(xhr) {
+            if (xhr.status >= 300) {
+                me.failed = true;
+                me.success = me.loading = false;
+                me.errorStatus = xhr.status;
+                me.errorBody = xhr.responseText;
+                m.redraw();
+            }
+            return xhr.responseText;
+        };
+        me.go = function() {
+            me = me;
+            me.loading = true;
+            me.success = me.failed = false;
+            m.request(me.opts).then(function(mydata) {
+                me.success = true;
+                me.failed = me.loading = false;
+                me.data = mydata;
+                m.redraw();
+            });
+        };
+    };
+};
+********/
--- app/sbg.js
+++ app/sbg.js
@@ -1,7 +1,8 @@
-/* global m, require */
+/* global require, module */
 
-var m = require("mithril");
+var m           = require("mithril");
 var Credentials = require("credentials");
+var Request     = require("request");
 
 var API_URL = "http://127.0.0.1:4000/api";
 
@@ -65,81 +66,6 @@ function formatDate(age, year, month, day) {
 function cmp(a, b) {
     return a > b ? 1 : a < b ? -1 : 0;
 }
-
-var Request = (function() {
-    var failFn = (resp, errorComponent) => {
-        var resolvedComponent = errorComponent || LoginScreen;
-        if (resp === null) {
-            resolvedComponent.setError("The server appears to be down. Please try again later.");
-        } else {
-            resolvedComponent.setError(resp.errors);
-        }
-        if (errorComponent) {
-            m.route();
-        } else {
-            m.route("/login");
-        }
-    };
-
-    return {
-        get: function(url, successFn, errorComponent) {
-            var opts = { method: "GET", url: url };
-            if (Credentials.token()) {
-                opts.config = function(xhr) { xhr.setRequestHeader("authorization", "Token token=" + Credentials.token()); };
-            }
-            return m.request(opts).then(
-                resp => successFn(resp),
-                resp => failFn(resp, errorComponent)
-            );
-        },
-
-        post: function(url, data, successFn, errorComponent) {
-            var opts = { method: "POST", url: url, data: data };
-            if (Credentials.token()) {
-                opts.config = function(xhr) { xhr.setRequestHeader("authorization", "Token token=" + Credentials.token()); };
-            }
-            return m.request(opts).then(
-                resp => successFn(resp),
-                resp => failFn(resp, errorComponent)
-            );
-        }
-    };
-}());
-
-/*******
-// courtesy http://ratfactor.com/daves-guide-to-mithril-js
-var requestWrapper = function(opts) {
-    return new function() {
-        var me = this;
-        me.opts = opts;
-        me.success = me.loading = me.failed = false;
-        me.errorStatus = me.errorBody = "";
-        me.data = null;
-        me.opts.background = true;
-        me.opts.extract = function(xhr) {
-            if (xhr.status >= 300) {
-                me.failed = true;
-                me.success = me.loading = false;
-                me.errorStatus = xhr.status;
-                me.errorBody = xhr.responseText;
-                m.redraw();
-            }
-            return xhr.responseText;
-        };
-        me.go = function() {
-            me = me;
-            me.loading = true;
-            me.success = me.failed = false;
-            m.request(me.opts).then(function(mydata) {
-                me.success = true;
-                me.failed = me.loading = false;
-                me.data = mydata;
-                m.redraw();
-            });
-        };
-    };
-};
-********/
 
 var Pie = {
     view: function(ctrl, size, n, nPainted, nOwned) {
@@ -471,6 +397,8 @@ var LoginScreen = function() {
         }
     };
 }();
+
+module.exports.LoginScreen = LoginScreen;
 
 //==================================================================================================================================
 var ScenarioListScreen = function() {
