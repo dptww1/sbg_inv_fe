958bbb6d048b14b1caed5c07b0d7ac633dc5e2c6
Dave Townsend <townsend@patriot.net>
2016-10-30 11:32:33 -0400

    RegisterScreen is modularized

---
+++ app/register.js
@@ -0,0 +1,89 @@
+/* global module require */
+
+var m           = require("mithril");
+var Credentials = require("credentials");
+var K           = require("constants");
+var Request     = require("request");
+
+//========================================================================
+var RegisterScreen = function() {
+    var errors = m.prop("");
+
+    var login = () => {
+        Request.post(K.API_URL + "/sessions",
+                     { user: { email: Credentials.email(), password: Credentials.password() } },
+                     resp => {
+                         Credentials.token(resp.data.token);
+                         console.log(resp.data.token);
+                         m.route("/scenarios");
+                     },
+                     RegisterScreen);
+    };
+
+    var register = () => {
+        Request.post(K.API_URL + "/users",
+                     { user: { name: Credentials.name(), email: Credentials.email(), password: Credentials.password() } },
+                     login,
+                     RegisterScreen);
+    };
+
+    var errorText = () => {
+        var msgs = [];
+        var errObj = errors().errors;
+        for (var key in errObj) {
+            msgs.push(key + ": " + errObj[key].join(", "));
+        }
+        return m.trust(msgs.join("<br/>"));
+    };
+
+    return {
+        name: m.prop(),
+        email: m.prop(),
+        password: m.prop(),
+        token: m.prop(),
+
+        setError(str) {
+            if (typeof(str) === "string") {
+                errors({errors: {"Error": [str]}});
+            } else {
+                errors({errors: str});
+            }
+        },
+
+        view(ctrl) {
+            return [
+                m(require("header")),
+                m(require("nav"), "Login"),
+                errors() ? m("div.errors", errorText()) : null,
+                m("div.main-content", [
+                    m("table", [
+                        m("tr", [
+                            m("td", "Name"),
+                            m("td", [
+                                m("input[type=text][name=name]", { onchange: m.withAttr("value", Credentials.name) })
+                            ])
+                        ]),
+                        m("tr", [
+                            m("td", "Email"),
+                            m("td", [
+                                m("input[type=text][name=email]", { onchange: m.withAttr("value", Credentials.email) })
+                            ])
+                        ]),
+                        m("tr", [
+                            m("td", "Password"),
+                            m("td", [
+                                m("input[type=password][name=password]", { onchange: m.withAttr("value", Credentials.password) })
+                            ])
+                        ]),
+                        m("tr", [
+                            m("td", ""),
+                            m("button[value=Sign In][name=signin]", { onclick: () => register() }, "Sign Up!")
+                        ])
+                    ])
+                ])
+            ];
+        }
+    };
+}();
+
+module.exports = RegisterScreen;
--- app/sbg.js
+++ app/sbg.js
@@ -1,13 +1,6 @@
-/* global require, module */
+/* global require */
 
-var m           = require("mithril");
-var Credentials = require("credentials");
-var Header      = require("header");
-var K           = require("constants");
-var Nav         = require("nav");
-var Request     = require("request");
-var ScenarioListScreen = require("scenario-list");
-
+var m = require("mithril");
 
 //==================================================================================================================================
 var MainScreen = {
@@ -19,93 +12,12 @@ var MainScreen = {
     }
 };
 
-//==================================================================================================================================
-var RegisterScreen = function() {
-    var errors = m.prop("");
-
-    var login = () => {
-        Request.post(K.API_URL + "/sessions",
-                     { user: { email: Credentials.email(), password: Credentials.password() } },
-                     resp => {
-                         Credentials.token(resp.data.token);
-                         console.log(resp.data.token);
-                         m.route("/scenarios");
-                     },
-                     RegisterScreen);
-    };
-
-    var register = () => {
-        Request.post(K.API_URL + "/users",
-                     { user: { name: Credentials.name(), email: Credentials.email(), password: Credentials.password() } },
-                     login,
-                     RegisterScreen);
-    };
-
-    var errorText = () => {
-        var msgs = [];
-        var errObj = errors().errors;
-        for (var key in errObj) {
-            msgs.push(key + ": " + errObj[key].join(", "));
-        }
-        return m.trust(msgs.join("<br/>"));
-    };
-
-    return {
-        name: m.prop(),
-        email: m.prop(),
-        password: m.prop(),
-        token: m.prop(),
-
-        setError(str) {
-            if (typeof(str) === "string") {
-                errors({errors: {"Error": [str]}});
-            } else {
-                errors({errors: str});
-            }
-        },
-
-        view(ctrl) {
-            return [
-                m(Header),
-                m(Nav, "Login"),
-                errors() ? m("div.errors", errorText()) : null,
-                m("div.main-content", [
-                    m("table", [
-                        m("tr", [
-                            m("td", "Name"),
-                            m("td", [
-                                m("input[type=text][name=name]", { onchange: m.withAttr("value", Credentials.name) })
-                            ])
-                        ]),
-                        m("tr", [
-                            m("td", "Email"),
-                            m("td", [
-                                m("input[type=text][name=email]", { onchange: m.withAttr("value", Credentials.email) })
-                            ])
-                        ]),
-                        m("tr", [
-                            m("td", "Password"),
-                            m("td", [
-                                m("input[type=password][name=password]", { onchange: m.withAttr("value", Credentials.password) })
-                            ])
-                        ]),
-                        m("tr", [
-                            m("td", ""),
-                            m("button[value=Sign In][name=signin]", { onclick: () => register() }, "Sign Up!")
-                        ])
-                    ])
-                ])
-            ];
-        }
-    };
-}();
-
 m.route.mode = "hash";
-m.route(document.getElementById("mainDiv"), "/", {
+m.route(document.getElementById("mainDiv"), "/scenarios", {
     "/"              : MainScreen,
     "/scenarios/:id" : require("scenario-details"),
     "/scenarios"     : require("scenario-list"),
     "/inventory"     : require("inventory"),
     "/login"         : require("login-screen"),
-    "/register"      : RegisterScreen
+    "/register"      : require("register")
 });
