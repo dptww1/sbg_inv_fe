de4eed1d7b9e0e3ce6b0d28b5deb2700be951344
Dave Townsend <townsend@patriot.net>
2016-10-30 10:47:37 -0400

    StarRating modularized

--- app/sbg.js
+++ app/sbg.js
@@ -6,6 +6,7 @@ var Header      = require("header");
 var K           = require("constants");
 var Pie         = require("pie");
 var Request     = require("request");
+var StarRating  = require("star-rating");
 
 function formatDate(age, year, month, day) {
     var a = [ ["", "FA", "SA", "TA"][age || 0] ];
@@ -106,62 +107,6 @@ var MainScreen = {
                  ]);
     }
 };
-
-//==================================================================================================================================
-var StarRating = function() {
-    var CELL_WIDTH = 16;
-
-    var highlightClassName = function(idx, userRating) {
-        return idx == userRating ? "rating-star-highlight" : "";
-    };
-
-    var ratingSpanWidth = function(idx, rating) {
-        if (idx <= rating) {
-            return "100%";
-        }
-        return Math.min(1 + ((rating - (idx - 1)) * (CELL_WIDTH - 2)), CELL_WIDTH);
-    };
-
-    var updateRating = function(scenario, newRating) {
-        Request.post(K.API_URL + "/userscenarios",
-                     { user_scenario: { scenario_id: scenario.id, rating: newRating } },
-                     resp => {
-                         scenario.rating = resp.avg_rating;
-                         scenario.user_scenario.rating = newRating;
-                         scenario.num_votes = resp.num_votes;
-                     });
-    };
-
-    return {
-        view: function(ctrl, isActive, scenario) {
-            var id = scenario.id;
-            var rating = scenario.rating;
-            var userRating = scenario.user_scenario.rating;
-            var votes = scenario.num_votes;
-            //console.log("id: " + id + ", rating: " + rating + ", userRating: " + userRating + ", votes: " + votes);
-            rating = Math.max(Math.min(rating, 5), 0);
-            var ratingCeiling = Math.ceil(rating);
-            if (!isActive) {
-                userRating = -1; // don't highlight anything
-            }
-            return m("div", { class: "rating" + (isActive ? " active" : "") }, [1, 2, 3, 4, 5].map(function(n) {
-                return m("div.rating-star-container",
-                         { onclick: function(ev) { if (isActive) { updateRating(scenario, n); } } },
-                         [
-                             m("div", { class: "rating-star " + highlightClassName(n, userRating) }, [
-                                 m.trust("&#9734;"),
-                                 n <= ratingCeiling ? m("div", {
-                                                          class: "rating-star-inner " + highlightClassName(n, userRating),
-                                                          style: "width:" + ratingSpanWidth(n, rating) + "px"
-                                                        },
-                                                        m.trust("&#9733;"))
-                                                    : null
-                             ])
-                         ]);
-            }).concat(votes > 0 ? m("span.votes", "(" + votes + ")") : null));
-        }
-    };
-}();
 
 //==================================================================================================================================
 var RegisterScreen = function() {
