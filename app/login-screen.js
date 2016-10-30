b5acc69f6622031b355bfcda8ff43f99ffa02fb5
Dave Townsend <townsend@patriot.net>
2016-10-30 11:27:16 -0400

    LoginScreen is modularized

---
+++ app/login-screen.js
@@ -0,0 +1,62 @@
+/* global require module */
+
+var m           = require("mithril");
+var Credentials = require("credentials");
+var K           = require("constants");
+var Request     = require("request");
+
+//========================================================================
+var LoginScreen = function() {
+    var errors = m.prop("");
+
+    var login = () => {
+        Request.post(K.API_URL + "/sessions",
+                     { user: { email: Credentials.email(), password: Credentials.password() } },
+                     resp => {
+                         Credentials.token(resp.data.token);
+                         Credentials.name(resp.data.name);
+                         console.log(resp.data.token);
+                         m.route("/scenarios");
+                     });
+    };
+
+    return {
+        email: m.prop(),
+        password: m.prop(),
+        token: m.prop(),
+
+        setError(str) {
+            errors({ errors: str});
+        },
+
+        view(ctrl) {
+            return [
+                m(require("header")),
+                m(require("nav"), "Login"),
+                errors() ? m("div.errors", errors().errors) : null,
+                m("div.main-content", [
+                    m("table", [
+                        m("tr", [
+                            m("td", "Email"),
+                            m("td", [
+                                m("input[type=text][name=email]", { onchange: m.withAttr("value", Credentials.email) })
+                            ])
+                        ]),
+                        m("tr", [
+                            m("td", "Password"),
+                            m("td", [
+                                m("input[type=password][name=password]", { onchange: m.withAttr("value", Credentials.password) })
+                            ])
+                        ]),
+                        m("tr", [
+                            m("td", ""),
+                            m("button[value=Sign In][name=signin]", { onclick: () => login() }, "Sign In!")
+                        ])
+                    ])
+                ])
+            ];
+        }
+    };
+}();
+
+module.exports = LoginScreen;
--- app/sbg.js
+++ app/sbg.js
@@ -100,68 +100,12 @@ var RegisterScreen = function() {
     };
 }();
 
-//==================================================================================================================================
-var LoginScreen = function() {
-    var errors = m.prop("");
-
-    var login = () => {
-        Request.post(K.API_URL + "/sessions",
-                     { user: { email: Credentials.email(), password: Credentials.password() } },
-                     resp => {
-                         Credentials.token(resp.data.token);
-                         Credentials.name(resp.data.name);
-                         console.log(resp.data.token);
-                         m.route("/scenarios");
-                     });
-    };
-
-    return {
-        email: m.prop(),
-        password: m.prop(),
-        token: m.prop(),
-
-        setError(str) {
-            errors({ errors: str});
-        },
-
-        view(ctrl) {
-            return [
-                m(Header),
-                m(Nav, "Login"),
-                errors() ? m("div.errors", errors().errors) : null,
-                m("div.main-content", [
-                    m("table", [
-                        m("tr", [
-                            m("td", "Email"),
-                            m("td", [
-                                m("input[type=text][name=email]", { onchange: m.withAttr("value", Credentials.email) })
-                            ])
-                        ]),
-                        m("tr", [
-                            m("td", "Password"),
-                            m("td", [
-                                m("input[type=password][name=password]", { onchange: m.withAttr("value", Credentials.password) })
-                            ])
-                        ]),
-                        m("tr", [
-                            m("td", ""),
-                            m("button[value=Sign In][name=signin]", { onclick: () => login() }, "Sign In!")
-                        ])
-                    ])
-                ])
-            ];
-        }
-    };
-}();
-
-module.exports.LoginScreen = LoginScreen;
-
 m.route.mode = "hash";
 m.route(document.getElementById("mainDiv"), "/", {
     "/"              : MainScreen,
     "/scenarios/:id" : require("scenario-details"),
     "/scenarios"     : require("scenario-list"),
     "/inventory"     : require("inventory"),
-    "/login"         : LoginScreen,
+    "/login"         : require("login-screen"),
     "/register"      : RegisterScreen
 });
