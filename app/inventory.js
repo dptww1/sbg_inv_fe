e77629391ee3f58a6eca27e50edbbb368648a5ee
Dave Townsend <townsend@patriot.net>
2017-01-09 18:24:01 -0500

    Basic inventory list

--- app/inventory.js
+++ app/inventory.js
@@ -3,13 +3,62 @@
 var InventoryScreen = {};
 module.exports = InventoryScreen;
 
-var m = require("mithril");
+var m       = require("mithril");
+var K       = require("constants");
+var Request = require("request");
+
+var armyId = "";
+var figuresMap = {};
+
+//========================================================================
+var figureListByType = function(title, list) {
+    if (list.length === 0) {
+        return null;
+    }
+
+    return m("div.inventory-section", [
+               m("div.title", title),
+               m("div.list", list.map(fig => {
+                   return m("div.name", fig["name"]);
+               }))
+             ]);
+};
+
+//========================================================================
+var armyDetails = function() {
+    if (armyId == "") {
+        return null;
+    }
+
+    return [
+        figureListByType("Characters", figuresMap.heroes.filter(fig => fig.unique)),
+        figureListByType("Heroes", figuresMap.heroes.filter(fig => !fig.unique)),
+        figureListByType("Warriors", figuresMap.warriors),
+        figureListByType("Monsters", figuresMap.monsters),
+        figureListByType("Siege Equipment", figuresMap.siegers)
+    ];
+};
+
+//========================================================================
+var updateArmyDetails = function(ev) {
+    armyId = ev.target.value;
+    Request.get("/faction/" + armyId,
+                resp => {
+                    figuresMap = resp.data;
+                });
+}
 
 //========================================================================
 InventoryScreen.view = () => {
     return [
         m(require("header")),
         m(require("nav"), "Inventory"),
-        m("div.main-content", "*** Inventory ***")
+        m("div.main-content inventory-main-content", [
+            m("select.faction", { onchange: ev => updateArmyDetails(ev) }, [
+                  m("option", { value: "" }, "-- Select an Army --"),
+                  Object.keys(K.FACTION_INFO).map((k, i) => m("option", { value: i }, K.FACTION_INFO[k].name))
+              ])
+          ]),
+        armyDetails()
     ];
 };
