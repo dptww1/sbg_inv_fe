af20623661ae36c0d0be9983ea8a72327b543988
Dave Townsend <townsend@patriot.net>
2017-09-05 18:41:44 -0400

    Style fixes. Pie charts show titles.

--- app/pie.js
+++ app/pie.js
@@ -1,8 +1,51 @@
 /* global module require */
 
-var m = require("mithril");
+const m = require("mithril");
 
-var Pie = {
+//========================================================================
+const slice = (circleAttrs, pctStart, pctEnd, fill) => {
+    // No slices at 0% or 100%
+    if (pctStart >= pctEnd || (pctStart == 0.0 && pctEnd == 1.0)) {
+        return null;
+    }
+
+    var pathParts = [];
+    pathParts.push("M" + circleAttrs.cx + "," + circleAttrs.cy);
+    pathParts.push("L" +
+                   (circleAttrs.cx + (Math.sin(pctStart * 2 * Math.PI) * circleAttrs.r)) + "," +
+                   (circleAttrs.cy - (Math.cos(pctStart * 2 * Math.PI) * circleAttrs.r)));
+    pathParts.push("A" + circleAttrs.r + "," + circleAttrs.r);
+    pathParts.push("0");  // x-axis rotate
+    pathParts.push((pctEnd - pctStart >= .5 ? "1" : "0") + ",1"); // long-arc, clockwise
+    pathParts.push((circleAttrs.cx + (Math.sin(pctEnd * 2 * Math.PI) * circleAttrs.r)) + "," +
+                   (circleAttrs.cy - (Math.cos(pctEnd * 2 * Math.PI) * circleAttrs.r)));
+    pathParts.push("z");
+
+    return m("path", {
+        d: pathParts.join(" "),
+        style: "fill: " + fill + "; fill-opacity: 1; stroke: black; stroke-width: 1"
+    });
+};
+
+//========================================================================
+const title = (pctPainted, pctOwned) => {
+    if (pctPainted === 0.0 && pctOwned === 0.0) {
+        return null;
+    }
+
+    const elts = [];
+    if (pctOwned !== 0.0) {
+        elts.push(Math.round(pctOwned * 100) + "% Owned");
+    }
+    if (pctPainted !== 0.0) {
+        elts.push(Math.round(pctPainted * 100) + "% Painted");
+    }
+
+    return m("title", elts.join(" / "));
+};
+
+//========================================================================
+const Pie = {
     view: function(vnode) {
         var size = vnode.attrs.size;
         var n = vnode.attrs.n;
@@ -19,35 +62,12 @@ var Pie = {
             circleAttrs.fill = '#bb0';
         }
 
-        return m("svg", { width: size, height: size }, [
-            m("circle", circleAttrs),
-            Pie.slice(circleAttrs, 0, pctPainted, "#0a0"),
-            Pie.slice(circleAttrs, pctPainted, pctOwned, "#bb0")
-        ]);
-    },
-
-    slice: function slice(circleAttrs, pctStart, pctEnd, fill) {
-        // No slices at 0% or 100%
-        if (pctStart >= pctEnd || (pctStart == 0.0 && pctEnd == 1.0)) {
-            return null;
-        }
-
-        var pathParts = [];
-        pathParts.push("M" + circleAttrs.cx + "," + circleAttrs.cy);
-        pathParts.push("L" +
-                       (circleAttrs.cx + (Math.sin(pctStart * 2 * Math.PI) * circleAttrs.r)) + "," +
-                       (circleAttrs.cy - (Math.cos(pctStart * 2 * Math.PI) * circleAttrs.r)));
-        pathParts.push("A" + circleAttrs.r + "," + circleAttrs.r);
-        pathParts.push("0");  // x-axis rotate
-        pathParts.push((pctEnd - pctStart >= .5 ? "1" : "0") + ",1"); // long-arc, clockwise
-        pathParts.push((circleAttrs.cx + (Math.sin(pctEnd * 2 * Math.PI) * circleAttrs.r)) + "," +
-                       (circleAttrs.cy - (Math.cos(pctEnd * 2 * Math.PI) * circleAttrs.r)));
-        pathParts.push("z");
-
-        return m("path", {
-            d: pathParts.join(" "),
-            style: "fill: " + fill + "; fill-opacity: 1; stroke: black; stroke-width: 1"
-        });
+        return m("svg", { width: size, height: size },
+                 m("g",
+                   title(pctPainted, pctOwned),
+                   m("circle", circleAttrs),
+                   slice(circleAttrs, 0, pctPainted, "#0a0"),
+                   slice(circleAttrs, pctPainted, pctOwned, "#bb0")));
     }
 };
 
