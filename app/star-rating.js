d5de54f52ec5791c959a3de17f628279d346876c
Dave Townsend <townsend@patriot.net>
2018-08-16 18:28:04 -0400

    Rework star-rating.js for style purposes

--- app/star-rating.js
+++ app/star-rating.js
@@ -3,68 +3,70 @@
 var m       = require("mithril");
 var Request = require("request");
 
+const CELL_WIDTH   = 16;
+const STAR_OUTLINE = "\u2606"; // ☆
+const STAR_SOLID   = "\u2605"; // ★
+
 //==================================================================================================================================
-var StarRating = function() {
-    var CELL_WIDTH = 16;
+const domStarSolid = (n, rating, userRating) => {
+    return m("div",
+             {
+                 class: `rating-star-inner ${highlightClassName(n, userRating)}`,
+                 style: `width: ${ratingSpanWidth(n, rating)}px`
+             },
+             STAR_SOLID);
+}
 
-    var highlightClassName = function(idx, userRating) {
-        return idx == userRating ? "rating-star-highlight" : "";
-    };
+//==================================================================================================================================
+const highlightClassName = (idx, userRating) => idx == userRating ? "rating-star-highlight" : "";
 
-    var ratingSpanWidth = function(idx, rating) {
-        if (idx <= rating) {
-            return "100%";
+//==================================================================================================================================
+const ratingSpanWidth = (idx, userRating) =>
+      idx <= userRating ? "100%" : Math.min(1 + ((userRating - (idx - 1)) * (CELL_WIDTH - 2)), CELL_WIDTH);
+
+//==================================================================================================================================
+const updateRating = (scenario, newRating, callback) => {
+    Request.post("/userscenarios",
+                 { user_scenario: { scenario_id: scenario.id, rating: newRating } },
+                 resp => {
+                     if (callback) {
+                         callback.call();
+                     } else {
+                         scenario.rating = resp.avg_rating;
+                         scenario.user_scenario.rating = newRating;
+                         scenario.num_votes = resp.num_votes;
+                         m.redraw(true);
+                     }
+                 });
+};
+
+//==================================================================================================================================
+const StarRating = {
+    view: function(vnode) {
+        const { isActive, scenario, callback } = vnode.attrs;
+        const { id, num_votes: votes } = scenario;
+        var { rating, user_scenario: { rating: userRating } } = scenario;
+
+        //console.log("id: " + id + ", rating: " + rating + ", userRating: " + userRating + ", votes: " + votes);
+
+        if (!isActive) {
+            userRating = -1; // don't highlight anything
         }
-        return Math.min(1 + ((rating - (idx - 1)) * (CELL_WIDTH - 2)), CELL_WIDTH);
-    };
+        rating = Math.max(Math.min(rating, 5), 0);
 
-    var updateRating = function(scenario, newRating, callback) {
-        Request.post("/userscenarios",
-                     { user_scenario: { scenario_id: scenario.id, rating: newRating } },
-                     resp => {
-                         if (callback) {
-                             callback.call();
-                         } else {
-                             scenario.rating = resp.avg_rating;
-                             scenario.user_scenario.rating = newRating;
-                             scenario.num_votes = resp.num_votes;
-                             m.redraw(true);
-                         }
-                     });
-    };
+        const ratingCeiling = Math.ceil(rating);
 
-    return {
-        view: function(vnode) {
-            var isActive = vnode.attrs.isActive;
-            var scenario = vnode.attrs.scenario;
-            var callback = vnode.attrs.callback;
-            var id = scenario.id;
-            var rating = scenario.rating;
-            var userRating = scenario.user_scenario.rating;
-            var votes = scenario.num_votes;
-            //console.log("id: " + id + ", rating: " + rating + ", userRating: " + userRating + ", votes: " + votes);
-            rating = Math.max(Math.min(rating, 5), 0);
-            var ratingCeiling = Math.ceil(rating);
-            if (!isActive) {
-                userRating = -1; // don't highlight anything
-            }
-            return m("div", { class: "rating" + (isActive ? " active" : "") }, [1, 2, 3, 4, 5].map(function(n) {
-                return m("div.rating-star-container",
-                         { onclick: function(ev) { if (isActive) { updateRating(scenario, n, callback); } } },
-                         [
-                             m("div", { class: "rating-star " + highlightClassName(n, userRating) }, [
-                                 "\u2606",
-                                 n <= ratingCeiling ? m("div", {
-                                                          class: "rating-star-inner " + highlightClassName(n, userRating),
-                                                          style: "width:" + ratingSpanWidth(n, rating) + "px"
-                                                        },
-                                                        "\u2605")
-                                                    : null
-                             ])
-                         ]);
-            }).concat(votes > 0 ? m("span.votes", "(" + votes + ")") : null));
-        }
-    };
-}();
+        return m("div",
+                 { class: `rating ${isActive ? "active" : ""}` },
+                 [1, 2, 3, 4, 5].map( n => {
+                     return m("div.rating-star-container",
+                              isActive ? { onclick: ev => updateRating(scenario, n, callback) } : {},
+                              m("div", { class: `rating-star ${highlightClassName(n, userRating)}` },
+                                STAR_OUTLINE,
+                                n <= ratingCeiling ? domStarSolid(n, rating, userRating) : null));
+                 }),
+                 votes > 0 ? m("span.votes", "(" + votes + ")") : null);
+    }
+};
 
 module.exports = StarRating;
