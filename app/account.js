8875eae742286e15f9886a7b25e764a301cd7142
Dave Townsend <townsend@patriot.net>
2018-08-19 16:25:52 -0400

    Style changes

--- app/account.js
+++ app/account.js
@@ -47,28 +47,28 @@ var AccountScreen = {
             message() ? m("div.message", message()) : null,
             m("div.main-content",
 
-              m("div", "Use this form to update your email address and/or password"),
+              m("div.text", "Use this form to update your email address and/or password"),
 
-              m("table",
+              m("p.text",
 
-                m("tr",
-                  m("td", "Name"),
-                  m("td", Credentials.name())),
+                m("table",
 
-                m("tr",
-                  m("td", "Email"),
-                  m("td",
-                    m("input[type=text][name=email][size=40]", { onchange: m.withAttr("value", Credentials.email) }))),
+                  m("tr",
+                    m("td", "Name"),
+                    m("td", Credentials.name())),
 
-                m("tr",
-                  m("td", "New Password"),
-                  m("td",
-                    m("input[type=password][name=password][size=40]", { onchange: m.withAttr("value", Credentials.password) })),
-                  m("td", m("span.field-note", "(leave empty to keep the same password)"))),
+                  m("tr",
+                    m("td", "Email"),
+                    m("td", m("input[type=text][name=email][size=40]", { onchange: m.withAttr("value", Credentials.email) }))),
+
+                  m("tr",
+                    m("td", "New Password"),
+                    m("td", m("input[type=password][name=password][size=40]", { onchange: m.withAttr("value", Credentials.password) })),
+                    m("td", m("span.field-note", "(leave empty to keep the same password)"))),
 
                 m("tr",
                   m("td", ""),
-                  m("button[value=Update][name=update]", { onclick: () => update() }, "Update My Account"))))
+                  m("button[value=Update][name=update]", { onclick: () => update() }, "Update My Account")))))
         ];
     }
 };
