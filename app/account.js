cba5395bacfba46e659b778f5e462c0da0198431
Dave Townsend <townsend@patriot.net>
2018-08-19 16:06:28 -0400

    Style update

--- app/account.js
+++ app/account.js
@@ -1,86 +1,76 @@
 /* global module require */
 
-var m           = require("mithril");
-var prop        = require("mithril/stream");
+const m           = require("mithril");
+const prop        = require("mithril/stream");
 
-var Credentials = require("credentials");
-var Request     = require("request");
+const Credentials = require("credentials");
+const Request     = require("request");
+
+const message = prop("");
 
 //========================================================================
-var AccountScreen = function() {
-    var errors = prop("");
-    var message = prop("");
+const errorText = () => {
+    var msgs = [];
+    var errObj = Request.errors().errors;
+    for (var key in errObj) {
+        msgs.push(key + ": " + errObj[key].join(", "));
+    }
+    return m.trust(msgs.join("<br/>"));
+};
 
-    var update = () => {
-        var paramMap = {};
-        if (Credentials.email()) {
-            paramMap["email"] = Credentials.email();
-        }
-        if (Credentials.password()) {
-            paramMap["password"] = Credentials.password();
-        }
-        Request.put("/users/" + Credentials.userId(),
-                    { user: paramMap },
-                    resp => {
-                        message("Account updated.");
-                        m.route.set("/account");
-                    },
-                    AccountScreen);
-    };
+//========================================================================
+const update = () => {
+    var paramMap = {};
 
-    var errorText = () => {
-        var msgs = [];
-        var errObj = errors().errors;
-        for (var key in errObj) {
-            msgs.push(key + ": " + errObj[key].join(", "));
-        }
-        return m.trust(msgs.join("<br/>"));
-    };
+    if (Credentials.email()) {
+        paramMap["email"] = Credentials.email();
+    }
 
-    return {
-        setError(str) {
-            if (typeof(str) === "string") {
-                errors({errors: {"Error": [str]}});
-            } else {
-                errors({errors: str});
-            }
-        },
+    if (Credentials.password()) {
+        paramMap["password"] = Credentials.password();
+    }
 
-        view() {
-            return [
-                m(require("header")),
-                m(require("nav"), { selected: "Account" }),
-                errors() ? m("div.errors", errorText()) : null,
-                message() ? m("div.message", message()) : null,
-                m("div.main-content", [
-                    m("div", "Use this form to update your email address and/or password"),
-                    m("table", [
-                        m("tr", [
-                            m("td", "Name"),
-                            m("td", Credentials.name())
-                        ]),
-                        m("tr", [
-                            m("td", "Email"),
-                            m("td", [
-                                m("input[type=text][name=email]", { onchange: m.withAttr("value", Credentials.email) })
-                            ])
-                        ]),
-                        m("tr", [
-                            m("td", "New Password"),
-                            m("td", [
-                                m("input[type=password][name=password]", { onchange: m.withAttr("value", Credentials.password) })
-                            ]),
-                            m("td", m("span.field-note", "(leave empty to keep the same password)"))
-                        ]),
-                        m("tr", [
-                            m("td", ""),
-                            m("button[value=Update][name=update]", { onclick: () => update() }, "Update My Account")
-                        ])
-                    ])
-                ])
-            ];
-        }
-    };
-}();
+    Request.put("/users/" + Credentials.userId(),
+                { user: paramMap },
+                resp => {
+                    message("Account updated.");
+                });
+};
+
+//========================================================================
+var AccountScreen = {
+    view: (/*vnode*/) => {
+        return [
+            m(require("header")),
+            m(require("nav"), { selected: "Account" }),
+            Request.errors() ? m("div.errors", errorText()) : null,
+            message() ? m("div.message", message()) : null,
+            m("div.main-content",
+
+              m("div", "Use this form to update your email address and/or password"),
+
+              m("table",
+
+                m("tr",
+                  m("td", "Name"),
+                  m("td", Credentials.name())),
+
+                m("tr",
+                  m("td", "Email"),
+                  m("td",
+                    m("input[type=text][name=email][size=40]", { onchange: m.withAttr("value", Credentials.email) }))),
+
+                m("tr",
+                  m("td", "New Password"),
+                  m("td",
+                    m("input[type=password][name=password][size=40]", { onchange: m.withAttr("value", Credentials.password) })),
+                  m("td", m("span.field-note", "(leave empty to keep the same password)"))),
+
+                m("tr",
+                  m("td", ""),
+                  m("button[value=Update][name=update]", { onclick: () => update() }, "Update My Account"))))
+        ];
+    }
+};
 
 module.exports = AccountScreen;
