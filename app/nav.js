03b5228936b50e1fdcf3c06916c58e5e02d7c875
Dave Townsend <townsend@patriot.net>
2018-08-20 18:49:29 -0400

    Style fixes

--- app/nav.js
+++ app/nav.js
@@ -7,39 +7,32 @@ const K                  = require("constants");
 //========================================================================
 const loggedInTabs = (loginActive) => {
     if (!Credentials.token()) {
-        return [];
+        return null;
     }
 
     return [
-        m("div.nav-header", { class: loginActive ? "nav-content-selected" : "" },  [
-            m("a[href=/account]", { oncreate: m.route.link }, [
-                m("span.icon", K.ICON_STRINGS.account),
-                Credentials.name()
-            ])
-          ]),
-        m("div.nav-header", [
-            m("a[href=/scenarios]", { oncreate: m.route.link, onclick: () => { Credentials.clear(); }  }, [
-                m("span.icon", K.ICON_STRINGS.log_out),
-                m("span.desktop", "Log Out")
-            ])
-          ])
+        m("div.nav-header", { class: loginActive ? "nav-content-selected" : "" },
+          m("a[href=/account]", { oncreate: m.route.link },
+            m("span.icon", K.ICON_STRINGS.account),
+            Credentials.name())),
+
+        m("div.nav-header",
+          m("a[href=/scenarios]", { oncreate: m.route.link, onclick: () => { Credentials.clear(); }  },
+            m("span.icon", K.ICON_STRINGS.log_out),
+            m("span.desktop", "Log Out")))
     ];
 };
 
 //========================================================================
 const loggedOutTabs = (loginActive) => {
     if (Credentials.token()) {
-        return [];
+        return null;
     }
 
-    return [
-        m("div.nav-header", { class: loginActive ? "nav-content-selected" : "" }, [
-            m("a[href=/login]", { oncreate: m.route.link }, [
-                m("span.icon", K.ICON_STRINGS.log_in),
-                m("span.desktop", "Login")
-            ])
-          ])
-    ];
+    return m("div.nav-header", { class: loginActive ? "nav-content-selected" : "" },
+             m("a[href=/login]", { oncreate: m.route.link },
+               m("span.icon", K.ICON_STRINGS.log_in),
+               m("span.desktop", "Login")));
 };
 
 //========================================================================
@@ -52,29 +45,25 @@ const Nav = {
         var scenarioListActive = vnode.attrs.selected == "Scenario List";
 
         return m("div.nav", [
-            m("div.nav-header", { class: scenarioListActive ? "nav-content-selected" : "" }, [
-                m("a", { href: "/scenarios", oncreate: m.route.link }, [
-                    m("span.icon", K.ICON_STRINGS.scenarios),
-                    m("span.desktop", "Scenarios")
-                ])
-            ]),
+            m("div.nav-header", { class: scenarioListActive ? "nav-content-selected" : "" },
+              m("a", { href: "/scenarios", oncreate: m.route.link },
+                m("span.icon", K.ICON_STRINGS.scenarios),
+                m("span.desktop", "Scenarios"))),
 
-            m("div.nav-header", { class: figureListActive ? "nav-content-selected" : "" }, [
-                m("a", { href: "/figures", oncreate: m.route.link }, [
-                    m("span.icon", K.ICON_STRINGS.figures),
-                    m("span.desktop", "Figures")
-                ])
-            ])
-        ].concat(loggedInTabs(accountActive))
-         .concat(loggedOutTabs(loginActive))
-         .concat([
-             m(".nav-header", { class: aboutActive ? "nav-content-selected" : "" }, [
-                 m("a", { href: "/about", oncreate: m.route.link }, [
-                     m("span.icon", K.ICON_STRINGS.about),
-                     m("span.desktop", "About")
-                 ])
-             ])
-         ])
+            m("div.nav-header", { class: figureListActive ? "nav-content-selected" : "" },
+              m("a", { href: "/figures", oncreate: m.route.link },
+                m("span.icon", K.ICON_STRINGS.figures),
+                m("span.desktop", "Figures"))),
+
+            loggedInTabs(accountActive),
+
+            loggedOutTabs(loginActive),
+
+            m(".nav-header", { class: aboutActive ? "nav-content-selected" : "" },
+              m("a", { href: "/about", oncreate: m.route.link },
+                m("span.icon", K.ICON_STRINGS.about),
+                m("span.desktop", "About")))
+         ]
         );
     }
 };
