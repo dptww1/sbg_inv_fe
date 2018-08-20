7f353a7a01dcbdeceb7c635e8f3870c994e6032d
Dave Townsend <townsend@patriot.net>
2018-08-20 18:27:08 -0400

    Style changes

--- app/forgot-password.js
+++ app/forgot-password.js
@@ -7,35 +7,34 @@ const Header  = require("header");
 const Nav     = require("nav");
 const Request = require("request");
 
+const email = prop();
+
 //========================================================================
-var ForgotPassword = function() {
-    var reset_password = () => {
-        Request.post("/reset-password",
-                     { user: { email: ForgotPassword.email() } },
-                     resp => {
-                         Request.errors({errors: "Your password has been reset.  You should receive an email with your new password shortly."});
-                     });
-    };
+const reset_password = () => {
+    Request.post("/reset-password",
+                 { user: { email: email() } },
+                 resp => {
+                     Request.errors({errors: "Your password has been reset.  You should receive an email with your new password shortly."});
+                 });
+};
 
-    return {
-        email: prop(),
-
-        view() {
-            return [
-                m(Header),
-                m(Nav, { selected: "Login" }),
-                Request.errors() ? m("div.errors", Request.errors().errors) : null,
-                m("div.main-content", [
-                    m("p.text", "Forgot your password?"),
-                    m("div", [
-                        m("p.text", "Enter your email address to reset your password:"),
-                        m("p.text", m("input.email[type=email][size=40]", { onchange: m.withAttr("value", ForgotPassword.email) })),
-                        m("p.text", m("button", { onclick: () => reset_password() }, "Reset"))
-                    ])
+//========================================================================
+var ForgotPassword = {
+    view() {
+        return [
+            m(Header),
+            m(Nav, { selected: "Login" }),
+            Request.errors() ? m("div.errors", Request.errors().errors) : null,
+            m("div.main-content", [
+                m("p.text", "Forgot your password?"),
+                m("div", [
+                    m("p.text", "Enter your email address to reset your password:"),
+                    m("p.text", m("input.email[type=email][size=40]", { onchange: m.withAttr("value", email) })),
+                    m("p.text", m("button", { onclick: () => reset_password() }, "Reset"))
                 ])
-            ];
-        }
-    };
-}();
+            ])
+        ];
+    }
+};
 
 module.exports = ForgotPassword;
