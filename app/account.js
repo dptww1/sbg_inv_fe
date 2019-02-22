0157c1c2f6f67dca1d6cc9a5aec0c90e8a775a66
Dave Townsend <townsend@patriot.net>
2019-02-22 16:33:10 -0500

    indent 4 space => 2 space

--- app/account.js
+++ app/account.js
@@ -10,58 +10,58 @@ const Request     = require("request");
 
 //========================================================================
 const update = () => {
-    var paramMap = {};
+  var paramMap = {};
 
-    if (Credentials.email()) {
-        paramMap["email"] = Credentials.email();
-    }
+  if (Credentials.email()) {
+    paramMap["email"] = Credentials.email();
+  }
 
-    if (Credentials.password()) {
-        paramMap["password"] = Credentials.password();
-    }
+  if (Credentials.password()) {
+    paramMap["password"] = Credentials.password();
+  }
 
-    Request.put("/users/" + Credentials.userId(),
-                { user: paramMap },
-                resp => {
-                    Request.messages("Account updated.");
-                });
+  Request.put("/users/" + Credentials.userId(),
+              { user: paramMap },
+              resp => {
+                Request.messages("Account updated.");
+              });
 };
 
 //========================================================================
 var AccountScreen = {
-    view: (/*vnode*/) => {
-        return [
-            m(Header),
-            m(Nav, { selected: "Account" }),
-            m("div.main-content",
+  view: (/*vnode*/) => {
+    return [
+      m(Header),
+      m(Nav, { selected: "Account" }),
+      m("div.main-content",
 
-              Credentials.isAdmin() ? m("div.text", "API URL: " + Request.apiUrl) : null,
+        Credentials.isAdmin() ? m("div.text", "API URL: " + Request.apiUrl) : null,
 
-              m("div.text", "Use this form to update your email address and/or password"),
+        m("div.text", "Use this form to update your email address and/or password"),
 
-              m("p.text",
-                m("table",
+        m("p.text",
+          m("table",
 
-                  m("tr",
-                    m("td", "Name"),
-                    m("td", Credentials.name())),
+            m("tr",
+              m("td", "Name"),
+              m("td", Credentials.name())),
 
-                  m("tr",
-                    m("td", "Email"),
-                    m("td", m("input[type=text][name=email][size=40]",
-                              { onchange: m.withAttr("value", Credentials.email), value: Credentials.email() }))),
+            m("tr",
+              m("td", "Email"),
+              m("td", m("input[type=text][name=email][size=40]",
+                        { onchange: m.withAttr("value", Credentials.email), value: Credentials.email() }))),
 
-                  m("tr",
-                    m("td", "New Password"),
-                    m("td", m("input[type=password][name=password][size=40]",
-                              { onchange: m.withAttr("value", Credentials.password), value: Credentials.password() })),
-                    m("td", m("span.field-note", "(leave empty to keep the same password)"))),
+            m("tr",
+              m("td", "New Password"),
+              m("td", m("input[type=password][name=password][size=40]",
+                        { onchange: m.withAttr("value", Credentials.password), value: Credentials.password() })),
+              m("td", m("span.field-note", "(leave empty to keep the same password)"))),
 
-                m("tr",
-                  m("td", ""),
-                  m("button[value=Update][name=update]", { onclick: () => update() }, "Update My Account")))))
-        ];
-    }
+            m("tr",
+              m("td", ""),
+              m("button[value=Update][name=update]", { onclick: () => update() }, "Update My Account")))))
+    ];
+  }
 };
 
 module.exports = AccountScreen;
