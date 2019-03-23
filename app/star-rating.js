d9f592dfad0b1be0b4a1d8aba3eefbb1283d8af6
Dave Townsend <townsend@patriot.net>
2019-03-23 11:17:03 -0400

    Two-space indents.

--- app/star-rating.js
+++ app/star-rating.js
@@ -1,6 +1,6 @@
 /* global module require */
 
-const m       = require("mithril");
+const m = require("mithril");
 
 const CELL_WIDTH   = 16;
 const STAR_OUTLINE = "\u2606"; // ☆
@@ -8,12 +8,12 @@ const STAR_SOLID   = "\u2605"; // ★
 
 //==================================================================================================================================
 const domStarSolid = (n, rating, userRating) => {
-    return m("div",
-             {
-                 class: `rating-star-inner ${highlightClassName(n, userRating)}`,
-                 style: `width: ${ratingSpanWidth(n, rating)}px`
-             },
-             STAR_SOLID);
+  return m("div",
+           {
+             class: `rating-star-inner ${highlightClassName(n, userRating)}`,
+             style: `width: ${ratingSpanWidth(n, rating)}px`
+           },
+           STAR_SOLID);
 }
 
 //==================================================================================================================================
@@ -27,28 +27,28 @@ const ratingSpanWidth = (idx, userRating) =>
 // m(StarRating, { id: <val>, active: <bool>, votes: <n>, rating: <n>, userRating: <n>, callback: fn(id, newRating) })
 //----------------------------------------------------------------------------------------------------------------------------------
 const StarRating = {
-    view: function(vnode) {
-        const { id, active, votes, callback } = vnode.attrs;
-        var   { rating, userRating = -1 }     = vnode.attrs;
+  view: function(vnode) {
+    const { id, active, votes, callback } = vnode.attrs;
+    var   { rating, userRating = -1 }     = vnode.attrs;
 
-        if (!active) {
-            userRating = -1; // don't highlight anything
-        }
-        rating = Math.max(Math.min(rating, 5), 0);
-
-        const ratingCeiling = Math.ceil(rating);
-
-        return m("div",
-                 { class: `rating ${active ? "active" : ""}` },
-                 [1, 2, 3, 4, 5].map( n => {
-                     return m("div.rating-star-container",
-                              active ? { onclick: ev => callback(id, n) } : {},
-                              m("div", { class: `rating-star ${highlightClassName(n, userRating)}` },
-                                STAR_OUTLINE,
-                                n <= ratingCeiling ? domStarSolid(n, rating, userRating) : null));
-                 }),
-                 votes > 0 ? m("span.votes", "(" + votes + ")") : null);
+    if (!active) {
+      userRating = -1; // don't highlight anything
     }
+    rating = Math.max(Math.min(rating, 5), 0);
+
+    const ratingCeiling = Math.ceil(rating);
+
+    return m("div",
+             { class: `rating ${active ? "active" : ""}` },
+             [1, 2, 3, 4, 5].map( n => {
+               return m("div.rating-star-container",
+                        active ? { onclick: ev => callback(id, n) } : {},
+                        m("div", { class: `rating-star ${highlightClassName(n, userRating)}` },
+                          STAR_OUTLINE,
+                          n <= ratingCeiling ? domStarSolid(n, rating, userRating) : null));
+             }),
+             votes > 0 ? m("span.votes", "(" + votes + ")") : null);
+  }
 };
 
 module.exports = StarRating;
