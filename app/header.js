7a587148ac66c3b11fa65c8bba7e3694a0750c48
Dave Townsend <townsend@patriot.net>
2021-11-27 18:15:08 -0500

    Updated header to show last updated date

--- app/header.js
+++ app/header.js
@@ -2,11 +2,41 @@
 
 var m = require("mithril");
 
+var Request = require("request");
+
+var lastUpdateDate = null;
+var nextUpdateDate = null;
+
+//======================================================================
+const getLastUpdateDate = _ => {
+  const now = new Date().getTime();
+  const nextCheckDate = new Date(parseInt(localStorage.getItem("header--lastUpdateCheck"), 10));
+  nextCheckDate.setDate(nextCheckDate.getDate() + 1);
+
+  if (lastUpdateDate === null || now > nextCheckDate) {
+    // Don't keep queueing up requests
+    lastUpdateDate = 0;
+
+    Request.get("/newsitem?n=1",
+                resp => {
+                  lastUpdateDate = resp.data[0].item_date;
+                  localStorage.setItem("header--lastUpdateCheck", now);
+                });
+  }
+
+  return lastUpdateDate;
+};
+
 //======================================================================
 var Header = {
   view: function() {
+    const updateDate = getLastUpdateDate();
+
     return m(".page-header", [
-      m(".title", "Middle Earth SBG Inventory")
+      m(".title", "Middle Earth SBG Inventory"),
+      updateDate
+        ? m(".last-update", m(m.route.Link, { href: "/about" }, "Last Update: " + updateDate))
+        : null
     ]);
   }
 };
