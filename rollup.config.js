7cbfd4de138a7758f666715abec8e6d81df9eeb6
Dave Townsend <townsend@patriot.net>
2024-01-01 20:22:20 -0500

    Add Rollup Terser plugin to create minimized JS

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,24 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
 
 export default {
   input: "app/sbg.js",
-  output: {
-    file: "public/app.js",
-    format: "iife",
-    generatedCode: "es2015",
-    sourcemap: true
-  },
+  output: [
+    {
+      file: "public/app.js",
+      format: "iife",
+      generatedCode: "es2015",
+      sourcemap: true
+    },
+    {
+      file: "public/app.min.js",
+      format: "iife",
+      generatedCode: "es2015",
+      sourcemap: true,
+      plugins: [ terser() ]
+    }
+  ],
   plugins: [
     commonjs(),
     nodeResolve()
