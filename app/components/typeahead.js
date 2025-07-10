f1fdf81a2567b401459d6b6cd045b1aa47e1789d
Dave Townsend <dave@davetownsend.org>
2025-07-09 22:50:34 -0400

    Fixed Typeahead not clearing input widget on [ESC]

--- app/components/typeahead.js
+++ app/components/typeahead.js
@@ -136,9 +136,9 @@ export const Typeahead = vnode => {
                  {
                    oncreate: vn => vn.dom.focus(),
                    placeholder: attrs.placeholder || 'Search...',
-                   onkeyup: handleKey
-                 },
-                 searchString),
+                   onkeyup: handleKey,
+                   value: searchString
+                 }),
                m(".suggestions",
                  m("ul",
                    data.suggestions.length === 0
