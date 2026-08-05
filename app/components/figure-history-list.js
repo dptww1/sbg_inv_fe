dbfbaebfe512f3ba58c7ed24601ccc95dcce69c0
Dave Townsend <dave@davetownsend.org>
2026-08-04 21:03:40 -0400

    History editing no longer allowed, and deletion restricted...
    ...to only the last record for a given figure.

--- app/components/figure-history-list.js
+++ app/components/figure-history-list.js
@@ -30,7 +30,20 @@ const updateHistory = (hist, callbackFn) => {
 };
 
 /**=======================================================================
- * Shows an editable list of user history records.
+ * Shows a list of user history records.  Only the last record is
+ * editable, and then only if all the records pertain to a single figure.
+ *
+ * The edit limitation is to facilitate "undo" functionality on the back end,
+ * where allowing arbitrary editing could result in nonsensical results.
+ * Consider a figure with only the following two records:
+ *    2026-04-02 Bought Unpainted 4
+ *    2025-10-20 Painted 4
+ * If the user deletes the first record, the back end undoes his/her inventory
+ * and resets the amount back to 0.  But then the second record makes no sense.
+ * More complicated cases are possible.  It seems easier just to disallow
+ * editing of any but the last record in the list.
+ *
+ * Note that the back end doesn't enforce the only-edit-last-record rule!
  *
  * Assumes the owning page has included `m(Dialog)` in its view.
  *
@@ -38,8 +51,9 @@ const updateHistory = (hist, callbackFn) => {
  *
  * @vattr {Object[]} list - array of user_figure_history records from the back end
  * @vattr {boolean} hideName - if `true`, the figure name is omitted from
- *     the list; useful on the {@link FigureDetails} page where the records
- *     are all referencing that current figure
+ *     the list; used on the {@link FigureDetails} page where the records
+ *     are all referencing that current figure. Edit tools are only available
+ *     when this flag is `true`
  * @vattr {function()} callbackFn - function called if any record in `list`
  *     is edited or deleted
  * @vattr {boolean} showTotals - if `true`, totals from `list` are shown
@@ -51,8 +65,14 @@ export const FigureHistoryList = {
       return m("p.text", "None");
     }
 
+    // Force sorting in date increasing order.  This is slightly hinky,
+    // in that `op_date` fields don't have to be accurate, so malicious
+    // users can force nonsensical order of operations. But ordinary
+    // users should be okay.
+    list.sort((a, b) => U.cmp(a.op_date, b.op_date));
+
     return m("table.striped.user-activity",
-             list.map(rec =>
+             list.map((rec, idx) =>
                       m("tr",
                         m("td.nobr", rec.op_date),
                         hideName
@@ -65,17 +85,14 @@ export const FigureHistoryList = {
                                 rec.amount > 1 ? U.pluralName(rec) : rec.name)),
                         m("td", K.USER_FIGURE_OPS[rec.op]),
                         m("td.numeric", rec.amount),
-                        m("td",
-                          m("span.action",
-                            {
-                              onclick: () => EditDialog.editHistory(rec, () => updateHistory(rec, callbackFn))
-                            },
-                            K.ICON_STRINGS.edit),
-                          m("span.action",
-                            {
-                              onclick: () => removeHistory(rec, callbackFn)
-                            },
-                            K.ICON_STRINGS.remove)),
+                        hideName && idx === (list.length - 1) // only allow edits on figure details page, and then only for the last record
+                          ? m("td",
+                              m("span.action",
+                                {
+                                  onclick: () => removeHistory(rec, callbackFn)
+                                },
+                                K.ICON_STRINGS.remove))
+                          : m("td"),
                         m("td", rec.notes))),
              showTotals
                ? m("tr.totals",
